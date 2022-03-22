7b3e383f6b8d8bc8bce611fffad1783646e0f6e6
wangzhitao <wangzhitao@co-mall.com>
2022-03-22 16:51:40 +0800

    feat: shanchu:

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const characterCount = (str, char) => str.split(char).length - 1;
-/**
- * 判断是否为空
- * @param obj
- * @returns true|false
- */
-export const isEmpty = (obj) =>
-  Reflect.ownKeys(obj).length === 0 && obj.constructor === Object;
