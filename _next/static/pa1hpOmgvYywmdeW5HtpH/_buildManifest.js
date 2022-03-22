b47cdf5ef54c66be613e39b46607e788d97c0779
wangzt-arch <61220020+wangzt-arch@users.noreply.github.com>
2022-03-22 04:57:37 +0000

    Deploying to gh-pages from @ wangzt-arch/myself@4d253d088124315b87247541fbbb0d78b11e8a99 🚀

--- _next/static/pa1hpOmgvYywmdeW5HtpH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-55f1fa2cc071e315.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/second":["static/chunks/pages/second-56b275297413b09c.js"],sortedPages:["/","/_app","/_error","/second"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
