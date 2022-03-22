e126129e67f2847fb10863b96cf456aa9cfde994
wangzt-arch <61220020+wangzt-arch@users.noreply.github.com>
2022-03-22 08:08:51 +0000

    Deploying to gh-pages from @ wangzt-arch/myself@52de125e6f42d2b40fd6a10f1775b6a3172eecff 🚀

---
+++ precache-manifest.a8f6c41e2fdcdab0802cfa86d67c2abd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70d8849ec18991aeb606cf3cff83152",
+    "url": "/myself/index.html"
+  },
+  {
+    "revision": "3608ef8288369329c4ec",
+    "url": "/myself/static/css/main.6ba72b62.chunk.css"
+  },
+  {
+    "revision": "b92dbf797e3748551e43",
+    "url": "/myself/static/js/2.eec29f80.chunk.js"
+  },
+  {
+    "revision": "3608ef8288369329c4ec",
+    "url": "/myself/static/js/main.06e66745.chunk.js"
+  },
+  {
+    "revision": "8e3bb94e21c8a73ad026",
+    "url": "/myself/static/js/runtime~main.5e9882c6.js"
+  }
+]);
