b08404d5b1deb0d5a999078a5fb73df8160a4ae9
wangzhitao <wangzhitao@co-mall.com>
2022-03-22 11:44:17 +0800

    chore: 添加header

---
+++ src/pages/_app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/Footer/index';
+import Header from '../components/Header/index';
+import '../styles/base.css';
+
+function App({ Component, pageProps }) {
+  return (
+    <div className="container">
+      <Header></Header>
+      <Component {...pageProps} />
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
