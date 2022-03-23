3b670dacdf11fd0dec7e7d2e610df84c53c086fa
wangzhitao <wangzhitao@co-mall.com>
2022-03-23 17:13:43 +0800

    chore: markdown

--- src/pages/docs/index.jsx
+++ src/pages/docs/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header'
 import docs from '../../docs/regex.md'
 import ReactMarkdown from 'react-markdown'
 
 function Docs() {
+    let [aaa, setAaa] = useState()
+    useEffect(() => {
+        fetch(docs).then(res => res.text()).then(text => {setAaa(text)})
+    }, [aaa])
     return (
         <div>
             <Header></Header>
             <div className="docs">
-            <ReactMarkdown children={docs}></ReactMarkdown>
+                <ReactMarkdown children={aaa}></ReactMarkdown>
             </div>
         </div>
     )
