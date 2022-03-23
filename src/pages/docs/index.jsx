5469868a3d74aeed95fecfb6131c2475d8b794a9
wangzhitao <wangzhitao@co-mall.com>
2022-03-23 16:24:37 +0800

    chore: markdown

--- src/pages/docs/index.jsx
+++ src/pages/docs/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header'
 import docs from '../../docs/regex.md'
-
+import ReactMarkdown from 'react-markdown'
 
 function Docs() {
     return (
         <div>
             <Header></Header>
             <div className="docs">
-
+            <ReactMarkdown children={docs}></ReactMarkdown>
             </div>
         </div>
     )
