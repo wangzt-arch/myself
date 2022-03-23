4dfff2af1e94bd255223387168fd8be6c74408e2
wangzhitao <wangzhitao@co-mall.com>
2022-03-23 11:24:26 +0800

    chore: line

--- src/components/Line/index.jsx
+++ src/components/Line/index.jsx
@@ -4,8 +4,9 @@ import "./index.scss";
 function Line() {
   return (
     <div className="line">
-      <input type="checkbox" id="toggle" checked />
+      <input type="checkbox" id="toggle"/>
       <section id="sect">
+      <label for="toggle" class="toggle">change view</label>
         <ul className="line-ul">
           <li className="line-li">swing</li>
           <li className="line-li">swing</li>
