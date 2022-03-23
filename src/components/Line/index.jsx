c34133168c393173f4cafd35f53c16d2fd01d21c
wangzhitao <wangzhitao@co-mall.com>
2022-03-23 11:39:16 +0800

    chore: linechange

--- src/components/Line/index.jsx
+++ src/components/Line/index.jsx
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 
 function Line() {
+  const [text, setText] = useState('swing')
   return (
     <div className="line">
-      <input type="checkbox" id="toggle"/>
+      <input type="checkbox" id="toggle" onChange={() => { text === 'swing' ? setText('learning') : setText('swing') }} />
       <section id="sect">
-      <label for="toggle" class="toggle">change view</label>
+        <label for="toggle" className="toggle">change view</label>
         <ul className="line-ul">
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
-          <li className="line-li">swing</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
+          <li className="line-li">{text}</li>
         </ul>
       </section>
     </div>
