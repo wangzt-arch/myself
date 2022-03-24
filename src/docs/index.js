215e9565dbd694e1df2d56513c7045b0223690a0
wangzhitao <wangzhitao@co-mall.com>
2022-03-24 16:23:47 +0800

    chore: raw

---
+++ src/docs/index.js
@@ -0,0 +1,8 @@
+import regex from "./regex.md";
+import regex2 from "./regex1.md";
+
+const docs = [
+  { title: "regex", value: regex },
+  { title: "regex1", value: regex2 },
+];
+export default docs;
--- src/pages/docs/index.jsx
+++ src/pages/docs/index.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import Header from '../../components/Header'
-import docs from '../../docs/regex.md'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import Header from "../../components/Header";
+import docs from "../../docs";
+import "./index.scss";
 
 function Docs() {
-    return (
-        <div>
-            <Header></Header>
-            <div className="docs">
-                <ReactMarkdown children={docs}></ReactMarkdown>
+  let [doc, setDoc] = useState(docs[0]);
+  const changeContent = (index) => {
+    setDoc(docs[index]);
+  };
+  return (
+    <div>
+      <Header></Header>
+      <div className="docs">
+        <div className="docs-title">
+          {docs.map((item, index) => (
+            <div
+              onClick={() =>changeContent(index)}
+              className="docs-title_item"
+              key={index}
+            >
+              {item.title}
             </div>
+          ))}
         </div>
-    )
+        <div className="docs-content">
+          <ReactMarkdown children={doc.value}></ReactMarkdown>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Docs
+export default Docs;
