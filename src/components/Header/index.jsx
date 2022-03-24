e83d3a784817dfcfb77e8f977e2422290b6ddc87
wangzhitao <wangzhitao@co-mall.com>
2022-03-24 11:21:46 +0800

    header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import WztButton from '../wzt-button'
+import logo from '../../images/github-icon.png'
 import "./index.css";
 
 function Header() {
@@ -14,6 +15,7 @@ function Header() {
                 </div>
             </div>
             <div className="header-right">
+                <a href="https://github.com/wangzt-arch"><img className="header-logo" src={logo} alt="" /></a>
                 <WztButton text='Home' onNavigate={() => navigate('/myself/home')}></WztButton>
                 <WztButton text='About' onNavigate={() => navigate('/myself/about')}></WztButton>
                 <WztButton text='Docs' onNavigate={() => navigate('/myself/docs')}></WztButton>
