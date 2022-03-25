3602228e4c1f0b767b5462c396ce1e73d8413b4a
wangzhitao <wangzhitao@co-mall.com>
2022-03-25 14:14:07 +0800

    feat: docs

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,8 +1,13 @@
 import regex from "./regex.md";
 import fp from "./fp.md";
+import harmony from "./harmonyos.md";
+import lets from "./let.md";
+
 
 const docs = [
   { title: "常用正则", value: regex },
+  { title: "变量声明", value: lets },
   { title: "函数式编程", value: fp },
+  { title: "鸿蒙OS", value: harmony },
 ];
 export default docs;
