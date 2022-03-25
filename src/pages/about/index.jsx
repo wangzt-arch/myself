68a01422ffe641bd58de4e3aefb841b7c18a9d6f
wangzhitao <wangzhitao@co-mall.com>
2022-03-25 17:16:33 +0800

    style: pdf

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Pdf from 'react-pdf-js'
 import Header from '../../components/Header';
 import CubeBox from '../../components/cube-box'
+import me from './pdf/myself.pdf'
 import './index.css'
 
 
 function About() {
+    let [page, setPage] = useState(1);
+    let [totalPage, setTotalPage] = useState(null);
+
+    const onDocumentComplete = (pages) => {
+        setTotalPage(pages)
+    }
+    const nextPage = () => {
+        page < totalPage && setPage(page + 1)
+    }
+    const prevPage = () => {
+        page > 1 && setPage(page - 1)
+    }
     return (
         <div className="about">
             <Header></Header>
             <div className="about-me">
                 <div className="about-me_pdf">
-                    
+                    <Pdf file={me} page={page} onDocumentComplete={onDocumentComplete}></Pdf>
+                </div>
+                <div className="pdf-button">
+                    <button className='pdf-button_prev' onClick={prevPage}>上一页</button>
+                    <button className='pdf-button_next' onClick={nextPage}>下一页</button>
                 </div>
             </div>
             <div className="cube-box-area">
