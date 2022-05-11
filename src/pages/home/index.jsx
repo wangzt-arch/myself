a0bf58d3f155d09e90c5aa8bdc8d4e7c2e739400
wangzhitao <1213332180@qq.com>
2022-05-11 11:32:59 +0800

    feat: 添加点击特效

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import Header from '../../components/Header';
-import Line from '../../components/Line'
-import './index.css'
-
+import React, { useRef } from "react";
+import Header from "../../components/Header";
+// import Line from '../../components/Line'
+import party from "party-js";
+import "./index.css";
 
 function Home() {
-    return (
-        <div className="home">
-            <Header></Header>
-            <div className="home-open">
-                <Line></Line>
-                <div className="home-content">
-                    
-                </div>
-            </div>
-        </div>
-    )
+  const partyRef = useRef();
+  function onParty() {
+    party.confetti(partyRef.current, {
+      count: party.variation.range(20, 40),
+    });
+  }
+  return (
+    <div className="home">
+      <Header></Header>
+      <div
+        className="home-open"
+        id="home-open"
+        ref={partyRef}
+        onClick={onParty}
+      >
+        {/* <Line></Line> */}
+        {/* <div className="home-content"></div> */}
+        还没想好写什么的部分,点击试试吧
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
