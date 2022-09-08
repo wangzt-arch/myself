2f0f74e5936f5f96672eaad0c4e56fea322fab06
DESKTOP-G0KKLD0\rsc <15731416521@163.com>
2022-09-08 10:01:35 +0800

    feat: SKILL

--- src/docs/index.js
+++ src/docs/index.js
@@ -3,13 +3,35 @@ import fp from "./fp.md";
 import harmony from "./harmonyos.md";
 import lets from "./let.md";
 import utils from "./utils.md";
+import skills from './skills.md';
+import commonInstructions from './skills'
 
 
 const docs = [
-  { title: "常用正则", value: regex },
-  { title: "变量声明", value: lets },
-  { title: "函数式编程", value: fp },
-  { title: "常用js工具", value: utils },
-  { title: "鸿蒙OS", value: harmony },
+    {
+        title: "常用正则",
+        value: regex
+    },
+    {
+        title: "变量声明",
+        value: lets
+    },
+    {
+        title: "函数式编程",
+        value: fp
+    },
+    {
+        title: "常用js工具",
+        value: utils
+    }, {
+        title: "鸿蒙OS",
+        value: harmony
+    }, {
+        title: "常用node指令",
+        value: commonInstructions
+    }, {
+        title: "掌握技能",
+        value: skills
+    },
 ];
 export default docs;
