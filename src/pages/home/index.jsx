ad596ca48c43c1deffeffca582eafb97b2d32a55
DESKTOP-G0KKLD0\rsc <15731416521@163.com>
2022-09-13 16:27:09 +0800

    fix: word

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,31 +1,30 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 import Header from "../../components/Header";
 // import Line from '../../components/Line'
 import party from "party-js";
 import "./index.css";
 
 function Home() {
-  const partyRef = useRef();
-  function onParty() {
-    party.confetti(partyRef.current, {
-      count: party.variation.range(20, 40),
-    });
-  }
-  return (
-    <div className="home">
-      <Header></Header>
-      <div
-        className="home-open"
-        id="home-open"
-        ref={partyRef}
-        onClick={onParty}
-      >
-        {/* <Line></Line> */}
-        {/* <div className="home-content"></div> */}
-        还没想好写什么的部分,点击试试吧
-      </div>
-    </div>
-  );
+    const partyRef = useRef();
+    function onParty() {
+        party.confetti(partyRef.current, {
+            count: party.variation.range(20, 40)
+        });
+    }
+    return (
+        <div className="home">
+            <Header></Header>
+            <div className="home-open" id="home-open"
+                ref={partyRef}
+                onClick={onParty}>
+                {/* <Line></Line> */}
+                {/* <div className="home-content"></div> */}
+                <div className="home-tip">
+                    构思中......点击试试吧
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
