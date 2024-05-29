27c089cad89c14f7d5e1dd36927b5b8292b1d4a4
wzt <1213332180@qq.com>
2024-05-29 17:48:16 +0800

    feat: 猫

--- src/pages/about/index2.jsx
+++ src/pages/about/index2.jsx
@@ -1,53 +1,50 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
+import Pdf from 'react-pdf-js'
 import Header from '../../components/Header';
-import CubeBox from '../../components/cube-box'
 import Loading from '../../components/Loading2';
+import CubeBox from '../../components/cube-box'
 import isPc from '../../utils'
 import me from './pdf/myself.pdf'
-import { usePdf } from '@mikecousins/react-pdf';
 import './index.css'
 
 
 function About() {
+  let [page, setPage] = useState(1);
+  let [totalPage, setTotalPage] = useState(null);
+  let [scale, setScale] = useState(1);
+  let [show, setShow] = useState(true);
   useEffect(() => {
-    isPc() ? setScale(1.05) : setScale(0.6)
+    isPc() ? setScale(1) : setScale(0.6)
   }, [])
 
-  const [page, setPage] = useState(1);
-  let [scale, setScale] = useState(1);
-  const canvasRef = useRef(null);
-
-  const { pdfDocument } = usePdf({
-    file: me,
-    page,
-    scale,
-    canvasRef,
-  });
-
-  const nextPage = () => {
-    page < pdfDocument.numPages && setPage(page + 1)
-  }
-  const prevPage = () => {
-    page > 1 && setPage(page - 1)
-  }
   const narrow = () => {
     setScale(scale - 0.1)
   }
   const enlarge = () => {
     setScale(scale + 0.1)
   }
+  const onDocumentComplete = (pages) => {
+    setTotalPage(pages)
+    // setTimeout(() => {
+    setShow(false)
+    // }, 1000);
 
-
-
+  }
+  const nextPage = () => {
+    page < totalPage && setPage(page + 1)
+  }
+  const prevPage = () => {
+    page > 1 && setPage(page - 1)
+  }
   return (
     <div className="about">
       <Header></Header>
       <div className="about-me">
         <div className='about-me__loading'>
-          <Loading show={!pdfDocument}></Loading>
+          <Loading show={show}></Loading>
         </div>
         <div>
-          <canvas className="about-me_pdf" ref={canvasRef} />
+          <Pdf className="about-me_pdf" file={me} scale={scale} page={page} onDocumentComplete={onDocumentComplete}></Pdf>
         </div>
       </div>
       <div className="pdf-button">
